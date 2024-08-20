a4755ac645b2f1db4e7a410c934df2c95ba15e3a
AbsolutelyBeginner <huy.businesscontact@gmail.com>
2024-08-20 15:27:53 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Basic User Authentication
+
+Huy and Kevin's project to learn what Docker is.
+
+## Installation
+
+Will update.
+
+## Goal:
+
+## Clone the repository
+git clone https://github.com/yourusername/your-repo-name.git
+
+## Navigate to the project directory
+cd your-repo-name
+
+## Install dependencies
+npm install
---
+++ client/src/App.jsx
@@ -0,0 +1,59 @@
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Registration from './pages/Registration/Registration';
+import NotFound from './pages/NotFound/NotFound';
+import Profile from './pages/Profile/Profile';
+import './App.scss'
+const Layout = () => {
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+
+};
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+
+      {
+        path: "/login",
+        element: <Login/>
+      },
+
+      {
+        path: "/registration",
+        element: <Registration/>
+      },
+
+      {
+        path: "/profile",
+        element: <Profile/>
+      },
+
+      {
+        path: "*",
+        element: <NotFound/>
+      }
+    ]
+  }
+]);
+function App() {
+
+  return (
+    <>
+      <div className='app-container'>
+        <RouterProvider router={router}/>
+      </div>
+    </>
+  )
+}
+
+export default App
