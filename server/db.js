81b54676fb247cc286481053483aad61607e78d2
HuyVoDN <huy.businesscontact@gmail.com>
2024-10-23 18:44:43 -0500

    changed from aws to azure

--- server/db.js
+++ server/db.js
@@ -1,14 +1,24 @@
 import mysql from 'mysql2';
 import {config} from 'dotenv';
-
+import fs from 'fs';
+import { isDataView } from 'util/types';
 config();
+
+const serverCA = [fs.readFileSync('./DigiCertGlobalRootCA.crt.pem')];
+if (serverCA == null)
+{
+    console.log("Error reading serverCA file");
+}
 
 const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS, // Ensure this matches the .env file
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: true,
+        ca: serverCA},
 }).promise();
 
 const connectDB = db.getConnection()
